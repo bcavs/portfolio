d889381802d81214d0ff83c930af8bc78956cd66
bcavs <cavenagh.ben@gmail.com>
2021-03-17 20:54:40 -0500

    set up project section

---
+++ src/components/ProjectsGroup/ProjectsGroup.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+//import { Test } from './ProjectsGroup.styles';
+
+const projects = [
+  {
+    name: "Project 1",
+    image: ""
+  }
+]
+
+const ProjectsGroup = (props) => (
+  <div className="projects-group">
+    
+  </div>
+);
+
+ProjectsGroup.propTypes = {
+  // bla: PropTypes.string,
+};
+
+ProjectsGroup.defaultProps = {
+  // bla: 'test',
+};
+
+export default ProjectsGroup;
