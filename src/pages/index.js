3bf01d60f6715c0a6fb23a1d632397052e0adc5a
bcavs <cavenagh.ben@gmail.com>
2021-08-31 10:49:15 -0500

    update years on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const IndexPage = () => (
   <Layout title="Home">
     <section id="personal-intro" className="flex justify-center container">
       <p className="text-lg md:text-2xl text-center leading-normal my-12 mx-8 max-w-4xl">
-        Hi! I'm <span className="text-electric-purple font-bold">Ben</span>, a <span className="text-electric-purple font-bold">fullstack web developer</span> that loves working with modern web development technologies like <span className="text-electric-purple">Gatsby</span>, <span className="text-electric-purple">Svelte</span>, <span className="text-electric-purple">React</span>, and <span className="text-electric-purple">React Native</span>. With over <span className="text-electric-purple font-bold">6 years of experience</span>, I have built projects ranging from mobile apps for startups to websites for international corporations.
+        Hi! I'm <span className="text-electric-purple font-bold">Ben</span>, a <span className="text-electric-purple font-bold">fullstack web developer</span> that loves working with modern web development technologies like <span className="text-electric-purple">Gatsby</span>, <span className="text-electric-purple">Svelte</span>, <span className="text-electric-purple">React</span>, and <span className="text-electric-purple">React Native</span>. With over <span className="text-electric-purple font-bold">7 years of experience</span>, I have built projects ranging from mobile apps for startups to websites for international corporations.
       </p>
     </section>
     <PreviousEmployers/>
